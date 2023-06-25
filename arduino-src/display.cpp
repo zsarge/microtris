ec60d31f840bbfe3c8938ccef0d426e551ab65e8
Zack Sargent <zackarywsargent@gmail.com>
2023-06-24 22:17:01 -0400

    move into .cpp and .hpp

---
+++ arduino-src/display.cpp
@@ -0,0 +1,105 @@
+#include "display.hpp"
+
+uint8_t Display::getCharacterBlock(uint8_t x, uint8_t y) {
+  if (y < 8) {
+    return x / character.pixelsWide;
+  } else {
+    return (x / character.pixelsWide) + 3;
+  }
+}
+
+Display::Display() {
+  for (int i = 0; i < board.charsWide * board.charsTall; i++) {
+    for (int y = 0; y < character.pixelsTall; y++) {
+      charBuffer[i][y] = 0;
+    }
+  }
+}
+
+void Display::printToSerial() {
+  printLineToSerial();
+
+  // print top row
+  for (uint8_t y = 0; y < character.pixelsTall; y++) {  // for every pixelRow in first row
+    for (uint8_t i = 0; i < 4; i++) {                   // print the columns
+      printByte(charBuffer[i][y]);
+    }
+    Serial.println();
+  }
+
+  // print bottom row
+  for (uint8_t y = 0; y < character.pixelsTall; y++) {  // for every pixelRow in first row
+    for (uint8_t i = 0; i < 4; i++) {                   // print the columns
+      printByte(charBuffer[i + 3][y]);
+    }
+    Serial.println();
+  }
+
+  printLineToSerial();
+}
+
+void Display::printToLcd(LiquidCrystal& lcd) {
+  for (int i = 0; i < board.charsTall * board.charsWide; i++) {
+    lcd.createChar(i, charBuffer[i]);
+  }
+
+  lcd.setCursor(0, 0);
+  for (uint8_t i = 0; i < board.charsWide; i++) {
+    lcd.write(i);
+  }
+
+  lcd.setCursor(0, 1);
+  for (uint8_t i = 0; i < board.charsWide; i++) {
+    lcd.write(i + 3);
+  }
+}
+
+void Display::draw(uint8_t x, uint8_t y) {
+  charBuffer[getCharacterBlock(x, y)][y % character.pixelsTall] |= getPattern(x);
+}
+
+void Display::erase(uint8_t x, uint8_t y) {
+  charBuffer[getCharacterBlock(x, y)][y % character.pixelsTall] &= getPattern(x, 0b11101111);
+}
+
+void Display::toggle(uint8_t x, uint8_t y) {
+  charBuffer[getCharacterBlock(x, y)][y % character.pixelsTall] ^= getPattern(x);
+}
+
+void Display::fill() {
+  for (uint8_t i = 0; i < board.charsTall * board.charsWide; i++) {
+    for (int8_t y = 0; y < character.pixelsTall; y++) {
+      charBuffer[i][y] = 0b11111;
+    }
+  }
+}
+
+void Display::clear() {
+  for (uint8_t i = 0; i < board.charsTall * board.charsWide; i++) {
+    for (int8_t y = 0; y < character.pixelsTall; y++) {
+      charBuffer[i][y] = 0;
+    }
+  }
+}
+
+void Display::printByte(byte val) {
+  for (int j = 4; j >= 0; j--) {
+    bool b = bitRead(val, j);
+    Serial.print(b);
+  }
+}
+
+byte Display::getPattern(uint8_t x) {
+  return getPattern(x, 0b10000);
+}
+
+byte Display::getPattern(uint8_t x, uint8_t initialState) {
+  return initialState >> x % character.pixelsWide;
+}
+
+void Display::printLineToSerial() {
+  for (uint8_t x = 0; x < board.pixelsWide; x++) {
+    Serial.print('-');
+  }
+  Serial.println();
+}
---
+++ arduino-src/display.hpp
@@ -0,0 +1,39 @@
+#ifndef HD_DISPLAY
+#define HD_DISPLAY
+
+#include <LiquidCrystal.h>
+#include <Arduino.h>
+
+class Display {
+public:
+  byte charBuffer[8][8];
+  struct {
+    uint8_t pixelsWide;
+    uint8_t pixelsTall;
+  } character = { 5, 8 };
+  struct {
+    uint8_t charsWide;
+    uint8_t charsTall;
+    uint8_t pixelsWide;
+    uint8_t pixelsTall;
+  } board = { 4, 2, 4 * 5, 2 * 8 };
+
+  Display();
+
+  void printToSerial();
+  void printToLcd(LiquidCrystal& lcd);
+
+  void draw(uint8_t x, uint8_t y);
+  void erase(uint8_t x, uint8_t y);
+  void toggle(uint8_t x, uint8_t y);
+  void fill();
+  void clear();
+
+private:
+  uint8_t getCharacterBlock(uint8_t x, uint8_t y);
+  void printByte(byte val);
+  byte getPattern(uint8_t x);
+  byte getPattern(uint8_t x, uint8_t initialState);
+  void printLineToSerial();
+};
+#endif
