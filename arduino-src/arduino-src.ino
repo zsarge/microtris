e32cf71fc7330ad0be5636db93bbce7fcc7b00e0
Zack Sargent <zackarywsargent@gmail.com>
2023-06-27 22:34:06 -0400

    add pieces as classes

--- arduino-src/arduino-src.ino
+++ arduino-src/arduino-src.ino
@@ -7,67 +7,309 @@ TODO:
 */
 
 enum Direction : uint8_t {
-  Up,
-  Down,
-  Left,
-  Right
+  North,
+  South,
+  East,
+  West
 };
 
-enum PieceName : uint8_t {
-  I = 0,
-  J,
-  L,
-  O,
-  S,
-  T,
-  Z
+// there's a bit of an overhead to having a piece
+// delegate to subclass via vtable, but it saves the
+// headache of a piece not knowing how big it is.
+class Piece {
+protected:
+  Direction direction;
+
+public:
+  void setDirection(Direction direction) {
+    this->direction = direction;
+  }
+  void rotateClockwise() {
+    switch (direction) {
+      case North:
+        direction = East;
+        break;
+      case East:
+        direction = South;
+        break;
+      case South:
+        direction = West;
+        break;
+      case West:
+        direction = North;
+        break;
+    }
+  }
+
+  virtual ~Piece();
+  virtual uint8_t getHeight();
+  virtual uint8_t getWidth();
+  virtual bool at(uint8_t, uint8_t);
 };
 
-// TODO: optimize into bits of bytes
-const bool pieces[7][4][3] = {
-  {
-    { 0, 1, 0 },  // I
-    { 0, 1, 0 },
-    { 0, 1, 0 },
-    { 0, 1, 0 },
-  },
-  {
-    { 0, 1, 0 },  // J
-    { 0, 1, 0 },
-    { 0, 1, 0 },
-    { 1, 1, 0 },
-  },
-  {
-    { 1, 0, 0 },  // L
-    { 1, 0, 0 },
-    { 1, 0, 0 },
-    { 1, 1, 0 },
-  },
-  {
-    { 1, 1, 0 },  // O
-    { 1, 1, 0 },
-    { 0, 0, 0 },
-    { 0, 0, 0 },
-  },
-  {
-    { 0, 1, 0 },  // S
-    { 1, 1, 0 },
-    { 1, 0, 0 },
-    { 0, 0, 0 },
-  },
-  {
-    { 0, 1, 0 },  // T
-    { 1, 1, 1 },
-    { 0, 0, 0 },
-    { 0, 0, 0 },
-  },
-  {
-    { 1, 0, 0 },  // Z
-    { 1, 1, 0 },
-    { 0, 1, 0 },
-    { 0, 0, 0 },
-  },
+Piece::~Piece() {}  // need to explicitly define this, to avoid linker errors
+uint8_t Piece::getHeight() {
+  return 0;  // should never get called
+}
+uint8_t Piece::getWidth() {
+  return 0;  // should never get called
+}
+bool Piece::at(uint8_t x, uint8_t y) {
+  return false;  // should never get called
+}
+
+class J : public Piece {
+public:
+  uint8_t getHeight() override {
+    switch (direction) {
+      case North:
+      case South:
+        return 3;
+      case East:
+      case West:
+        return 2;
+    }
+  }
+  uint8_t getWidth() override {
+    switch (direction) {
+      case North:
+      case South:
+        return 2;
+      case East:
+      case West:
+        return 3;
+    }
+  }
+  // is there a more efficent way to do this? probably
+  bool at(uint8_t x, uint8_t y) override {
+    if (x > getHeight() || y > getWidth()) return false;
+    switch (direction) {
+      case North:
+        return (bool[3][2]){
+          { 0, 1 },
+          { 0, 1 },
+          { 1, 1 },
+        }[y][x];
+      case East:
+        return (bool[2][3]){
+          { 1, 0, 0 },
+          { 1, 1, 1 },
+        }[y][x];
+      case South:
+        return (bool[3][2]){
+          { 1, 1 },
+          { 1, 0 },
+          { 1, 0 },
+        }[y][x];
+      case West:
+        return (bool[2][3]){
+          { 1, 1, 1 },
+          { 0, 0, 1 },
+        }[y][x];
+    }
+  }
 };
+
+class L : public Piece {
+public:
+  uint8_t getHeight() override {
+    switch (direction) {
+      case North:
+      case South:
+        return 3;
+      case East:
+      case West:
+        return 2;
+    }
+  }
+  uint8_t getWidth() override {
+    switch (direction) {
+      case North:
+      case South:
+        return 2;
+      case East:
+      case West:
+        return 3;
+    }
+  }
+
+  bool at(uint8_t x, uint8_t y) override {
+    if (x > getHeight() || y > getWidth()) return false;
+    switch (direction) {
+      case North:
+        return (bool[3][2]){
+          { 1, 0 },
+          { 1, 0 },
+          { 1, 1 },
+        }[y][x];
+      case East:
+        return (bool[2][3]){
+          { 1, 1, 1 },
+          { 1, 0, 0 },
+        }[y][x];
+      case South:
+        return (bool[3][2]){
+          { 1, 1 },
+          { 0, 1 },
+          { 0, 1 },
+        }[y][x];
+      case West:
+        return (bool[2][3]){
+          { 0, 0, 1 },
+          { 1, 1, 1 },
+        }[y][x];
+    }
+  }
+};
+
+
+class O : public Piece {
+public:
+  uint8_t getHeight() override {
+    return 2;
+  }
+  uint8_t getWidth() override {
+    return 2;
+  }
+  bool at(uint8_t x, uint8_t y) override {
+    if (x > getHeight() || y > getWidth()) return false;
+    return true;
+  }
+};
+
+class I : public Piece {
+public:
+  uint8_t getHeight() override {
+    switch (direction) {
+      case North:
+      case South:
+        return 4;
+      case East:
+      case West:
+        return 1;
+    }
+  }
+  uint8_t getWidth() override {
+    switch (direction) {
+      case North:
+      case South:
+        return 1;
+      case East:
+      case West:
+        return 4;
+    }
+  }
+
+  bool at(uint8_t x, uint8_t y) override {
+    if (x > getHeight() || y > getWidth()) return false;
+    return true;
+  }
+};
+
+class S : public Piece {
+public:
+  uint8_t getHeight() override {
+    switch (direction) {
+      case North:
+      case South:
+        return 3;
+      case East:
+      case West:
+        return 2;
+    }
+  }
+  uint8_t getWidth() override {
+    switch (direction) {
+      case North:
+      case South:
+        return 2;
+      case East:
+      case West:
+        return 3;
+    }
+  }
+
+  bool at(uint8_t x, uint8_t y) override {
+    if (x > getHeight() || y > getWidth()) return false;
+    switch (direction) {
+      case North:
+        return (bool[3][2]){
+          { 0, 1 },  // S
+          { 1, 1 },
+          { 1, 0 },
+        }[y][x];
+      case East:
+        return (bool[2][3]){
+          { 1, 1, 0 },
+          { 0, 1, 1 },
+        }[y][x];
+      case South:
+        return (bool[3][2]){
+          { 1, 0 },  // S
+          { 1, 1 },
+          { 0, 1 },
+        }[y][x];
+      case West:
+        return (bool[2][3]){
+          { 0, 1, 1 },
+          { 1, 1, 0 },
+        }[y][x];
+    }
+  }
+};
+
+class Z : public Piece {
+public:
+  uint8_t getHeight() override {
+    switch (direction) {
+      case North:
+      case South:
+        return 3;
+      case East:
+      case West:
+        return 2;
+    }
+  }
+  uint8_t getWidth() override {
+    switch (direction) {
+      case North:
+      case South:
+        return 2;
+      case East:
+      case West:
+        return 3;
+    }
+  }
+
+  bool at(uint8_t x, uint8_t y) override {
+    if (x > getHeight() || y > getWidth()) return false;
+    switch (direction) {
+      case North:
+        return (bool[3][2]){
+          { 0, 1 },  // Z
+          { 1, 1 },
+          { 1, 0 },
+        }[y][x];
+      case East:
+        return (bool[2][3]){
+          { 0, 1, 1 },
+          { 1, 1, 0 },
+        }[y][x];
+      case South:
+        return (bool[3][2]){
+          { 0, 1 },  // S
+          { 1, 1 },
+          { 1, 0 },
+        }[y][x];
+      case West:
+        return (bool[2][3]){
+          { 1, 1, 0 },
+          { 0, 1, 1 },
+        }[y][x];
+    }
+  }
+};
+
 
 class Minitris {
 private:
@@ -77,6 +319,8 @@ private:
   const static uint8_t width = 4 * 5;
   bool droppedBuffer[height][width];
 
+  // piece variables
+  Piece* piece;
   uint8_t x_offset;
   uint8_t y_offset;
 
@@ -84,6 +328,20 @@ private:
     x_offset = width / 2;
     y_offset = 0;
   }
+
+  void setNextPiece() {
+    delete piece;
+    piece = new J();
+  }
+
+  enum ButtonState : uint8_t {
+    Up,
+    Down,
+    Left,
+    Right,
+    Select,
+    None
+  };
 
 public:
 
@@ -95,10 +353,12 @@ public:
       for (uint8_t x = 0; x < width; x++)
         this->droppedBuffer[y][x] = false;
     resetToTop();
+    piece = new J();
   }
 
   ~Minitris() {
     delete this->display;
+    delete piece;
   }
 
   // for use in scanning through a piece to see its state
@@ -112,17 +372,73 @@ public:
     // - uint8_T y_offset
   }
 
-  void tick() {
-    if (y_offset < height - 4)
+  ButtonState readButton() {
+    int input = analogRead(0);
+    if (input > 1000) return None;  // guard clause for most common result
+    if (input < 50) return Right;
+    if (input < 195) return Up;
+    if (input < 380) return Down;
+    if (input < 555) return Left;
+    if (input < 790) return Select;
+    return None;  // not sure how this would happen
+  }
+
+  // void rotateCounterClockwise() {
+  //   switch (direction) {
+  //     case Up:
+  //     direction = Left
+  //   }
+  // }
+
+  void moveLeft() {
+    if (x_offset > 0)
+      x_offset--;
+  }
+
+  void moveRight() {
+    if (x_offset < width - piece->getWidth())
+      x_offset++;
+  }
+
+  void moveDown() {
+    if (y_offset < height - piece->getHeight())
       y_offset++;
+  }
+
+  void inputTick() {
+    switch (readButton()) {
+      case None: break;
+      case Up:
+        piece->rotateCounterClockwise();
+        break;
+      case Down:
+        piece->rotateClockwise();
+        break;
+      case Left:
+        moveLeft();
+        break;
+      case Right:
+        moveRight();
+        break;
+      case Select:
+        break;
+    }
+  }
+
+  void gameTick() {
+    moveDown();
   }
   /* draw() is in charge of rendering the piece,
    * and all of the dropped blocks, to the screen.
    */
   void draw() {
+    Serial.print("Piece height: ");
+    Serial.println(piece->getHeight());
+    Serial.print("Piece Width: ");
+    Serial.println(piece->getWidth());
     display->clear();
 
-    draw(S);
+    drawPiece();
 
     // for (uint8_t piece = PieceName::I; piece != PieceName::S; piece++) {
     //   draw(piece, piece * 5, 0);
@@ -136,13 +452,12 @@ public:
     // display->printToSerial();
   }
 
-  void draw(PieceName piece) {
-    uint8_t width = 3;
-    uint8_t height = 4;
 
-    for (uint8_t y = 0; y < height; y++) {
-      for (uint8_t x = 0; x < width; x++) {
-        display->set(x + x_offset, y + y_offset, pieces[piece][y][x]);
+
+  void drawPiece() {
+    for (uint8_t y = 0; y < piece->getHeight(); y++) {
+      for (uint8_t x = 0; x < piece->getWidth(); x++) {
+        display->set(x + x_offset, y + y_offset, piece->at(x, y));
       }
     }
   }
@@ -154,6 +469,7 @@ Minitris minitris = Minitris(&lcd);
 
 void setup() {
   Serial.begin(9600);  // open the serial port at 9600 bps:
+  Serial.println("Serial port initalized...");
   lcd.begin(16, 2);
 
   // create wall
@@ -169,7 +485,8 @@ void setup() {
 }
 
 void loop() {
-  minitris.tick();
+  minitris.inputTick();
+  minitris.gameTick();
   minitris.draw();
-  delay(1000);
+  delay(500);
 }
